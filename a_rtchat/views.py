f320e1adb2e27ea7ba70523533db9ee17c23b787
jatinswarnkar <jatin.swarnkar@perceptiviti.com>
2026-07-28 15:39:43 +0530

    Auto-create public-chat group if missing in database

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -9,7 +9,10 @@ from asgiref.sync import async_to_sync
 
 @login_required 
 def chat_view(request, chatroom_name='public-chat'):
-    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+    if chatroom_name == 'public-chat':
+        chat_group, created = ChatGroup.objects.get_or_create(group_name='public-chat')
+    else:
+        chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
     chat_messages = chat_group.chat_messages.all()[:30]
     form = ChatmessageCreateForm()
     
